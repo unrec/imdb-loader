a8d5973852cca229764765c5deda9705dab58917
unrec <andrei.troitskiy@gmail.com>
2021-07-20 11:28:03 +0300

    initial service added

---
+++ src/main/kotlin/com/unrec/imdb/search/Application.kt
@@ -0,0 +1,11 @@
+package com.unrec.imdb.search
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImdbSearchServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImdbSearchServiceApplication>(*args)
+}
