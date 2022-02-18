6b6ff2e6bc304ae7c26bcbf4717884e8222f56fd
unrec <andrei.troitskiy@gmail.com>
2022-02-18 09:40:49 +0300

    'camel' package removed

--- src/main/kotlin/com/unrec/imdb/search/camel/parser/FileParser.kt
+++
@@ -1,8 +0,0 @@
-package com.unrec.imdb.search.camel.parser
-
-import java.io.InputStream
-
-interface FileParser<T> {
-    fun parseRecords(data: String): List<T>
-    fun parseRecords(data: InputStream): List<T>
-}
