3f8cecb07a8311e44149f9cf2e38e856e4173fda
unrec <andrei.troitskiy@gmail.com>
2022-02-21 08:06:09 +0300

    JPA support removed from entities

--- src/main/kotlin/com/unrec/imdb/search/entity/BasicsEntity.kt
+++ src/main/kotlin/com/unrec/imdb/search/entity/BasicsEntity.kt
@@ -1,13 +1,6 @@
 package com.unrec.imdb.search.entity
 
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
-
-@Entity
-@Table(name = "basics")
-class BasicsEntity(
-    @Id
+data class BasicsEntity(
     val titleId: Long,
     val titleType: String,
     val primaryTitle: String,
@@ -17,19 +10,4 @@ class BasicsEntity(
     val endYear: Int?,
     val runtimeMinutes: Int?,
     val genres: String,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as BasicsEntity
-
-        if (titleId != other.titleId) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return titleId.hashCode()
-    }
-}
+)
--- src/main/kotlin/com/unrec/imdb/search/entity/CrewEntity.kt
+++ src/main/kotlin/com/unrec/imdb/search/entity/CrewEntity.kt
@@ -1,29 +1,7 @@
 package com.unrec.imdb.search.entity
 
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
-
-@Entity
-@Table(name = "crew")
-class CrewEntity(
-    @Id
+data class CrewEntity(
     val titleId: Long,
     val directors: String?,
     val writers: String?,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as CrewEntity
-
-        if (titleId != other.titleId) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return titleId.hashCode()
-    }
-}
+)
