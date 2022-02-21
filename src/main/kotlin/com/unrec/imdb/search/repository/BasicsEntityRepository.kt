6b5efe50bad8752de8bf018d5501468feae33e94
unrec <andrei.troitskiy@gmail.com>
2022-02-21 08:05:50 +0300

    BasicsEntityRepository removed

--- src/main/kotlin/com/unrec/imdb/search/repository/BasicsEntityRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.unrec.imdb.search.repository
-
-import com.unrec.imdb.search.entity.BasicsEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface BasicsEntityRepository : JpaRepository<BasicsEntity, Long>
